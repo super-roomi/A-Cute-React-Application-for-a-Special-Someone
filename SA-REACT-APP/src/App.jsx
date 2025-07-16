2190e3bfd318550e8b8b28ea130f9ea5945de7b9
_roomi <roomiisdead@gmail.com>
2025-07-16 19:33:28 +0300

    Updated with no node_modules

--- SA-REACT-APP/src/App.jsx
+++ SA-REACT-APP/src/App.jsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Nav from './Nav'
 import MainContent from './MainContent'
